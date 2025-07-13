dd851ba94e5f7de926b99597a83e223ea292467f
vvlssp <valesep2311@gmail.com>
2025-07-13 19:01:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,52 +1,58 @@
 const ramos = [
-  {
-    id: "bioquimica",
-    nombre: "Bioquímica",
-    creditos: 10,
-    requisitos: ["bio_microorganismos", "quimica_organica_ii"],
-  },
-  {
-    id: "quimica_organica_ii",
-    nombre: "Química Orgánica II",
-    creditos: 10,
-    requisitos: ["quimica_organica_i"],
-  },
-  {
-    id: "quimica_organica_i",
-    nombre: "Química Orgánica I",
-    creditos: 10,
-    requisitos: ["quimica_general_ii"],
-  },
-  {
-    id: "quimica_general_ii",
-    nombre: "Química General II",
-    creditos: 10,
-    requisitos: ["quimica_general_i", "lab_quimica_general"],
-  },
-  {
-    id: "quimica_general_i",
-    nombre: "Química General I",
-    creditos: 10,
-    requisitos: [],
-  },
-  {
-    id: "lab_quimica_general",
-    nombre: "Lab. Química General",
-    creditos: 10,
-    requisitos: [],
-  },
-  {
-    id: "bio_microorganismos",
-    nombre: "Biología de los Microorganismos",
-    creditos: 10,
-    requisitos: ["biologia_celula"],
-  },
-  {
-    id: "biologia_celula",
-    nombre: "Biología de la Célula",
-    creditos: 10,
-    requisitos: [],
-  }
+  // PRIMER AÑO
+  { id: "bioquimica_topicos", nombre: "Tópicos de Bioquímica", creditos: 5, requisitos: [] },
+  { id: "biologia_celula", nombre: "Biología de la Célula", creditos: 10, requisitos: [] },
+  { id: "lab_quimica_general", nombre: "Lab. Química General", creditos: 10, requisitos: [] },
+  { id: "quimica_general_i", nombre: "Química General I", creditos: 10, requisitos: [] },
+  { id: "pre_calculo", nombre: "Pre Cálculo", creditos: 10, requisitos: [] },
+  { id: "filosofia", nombre: "Filosofía: ¿Para qué?", creditos: 10, requisitos: [] },
+  { id: "fisica_ciencias", nombre: "Física para Ciencias", creditos: 10, requisitos: [] },
+  { id: "lab_fisica", nombre: "Lab. Física para Ciencias", creditos: 0, requisitos: [] },
+  { id: "quimica_general_ii", nombre: "Química General II", creditos: 10, requisitos: ["quimica_general_i", "lab_quimica_general"] },
+  { id: "calculo_i", nombre: "Cálculo I", creditos: 10, requisitos: ["pre_calculo"] },
+  { id: "teologico", nombre: "Teológico", creditos: 10, requisitos: [] },
+  { id: "electivo_1", nombre: "Electivo", creditos: 10, requisitos: [] },
+
+  // SEGUNDO AÑO
+  { id: "bases_fisicas", nombre: "Bases Físicas de los Procesos Biológicos", creditos: 10, requisitos: ["biologia_celula", "pre_calculo", "quimica_general_i", "fisica_ciencias"] },
+  { id: "bioestadistica", nombre: "Bioestadística", creditos: 10, requisitos: ["pre_calculo"] },
+  { id: "quimica_organica_i", nombre: "Química Orgánica I", creditos: 10, requisitos: ["quimica_general_ii"] },
+  { id: "quimica_analitica", nombre: "Química Analítica I", creditos: 10, requisitos: ["quimica_general_ii"] },
+  { id: "electivo_deportivo", nombre: "Electivo Deportivo", creditos: 10, requisitos: [] },
+  { id: "electivo_2", nombre: "Electivo", creditos: 10, requisitos: [] },
+  { id: "electivo_3", nombre: "Electivo", creditos: 10, requisitos: [] },
+  { id: "bioetica", nombre: "Bioética", creditos: 10, requisitos: ["biologia_celula"] },
+  { id: "bio_microorganismos", nombre: "Biología de los Microorganismos", creditos: 10, requisitos: ["biologia_celula"] },
+  { id: "quimica_organica_ii", nombre: "Química Orgánica II", creditos: 10, requisitos: ["quimica_organica_i"] },
+  { id: "analisis_instrumental", nombre: "Análisis Instrumental", creditos: 10, requisitos: ["quimica_analitica"] },
+  { id: "electivo_4", nombre: "Electivo", creditos: 10, requisitos: [] },
+  { id: "electivo_5", nombre: "Electivo", creditos: 10, requisitos: [] },
+
+  // TERCER AÑO
+  { id: "genetica_evolucion", nombre: "Genética y Evolución", creditos: 10, requisitos: ["bioestadistica", "biologia_celula"] },
+  { id: "fisiovegetal_biocel", nombre: "Fisiología y Bioquímica Vegetal o Biología y Fisiología Celular", creditos: 10, requisitos: ["bases_fisicas", "bio_microorganismos"] },
+  { id: "bioquimica", nombre: "Bioquímica", creditos: 10, requisitos: ["quimica_organica_ii", "bio_microorganismos"] },
+  { id: "lab_bioq_i", nombre: "Lab. Bioquímica I; Biología Celular", creditos: 5, requisitos: ["quimica_organica_ii", "analisis_instrumental"] },
+  { id: "lab_q_organica", nombre: "Lab. Química Orgánica", creditos: 10, requisitos: ["quimica_organica_ii"] },
+
+  { id: "genetica_molecular", nombre: "Genética Molecular", creditos: 10, requisitos: ["bioquimica"] },
+  { id: "lab_bioq_ii", nombre: "Lab. Bioquímica II Genética Molecular", creditos: 10, requisitos: ["bioquimica", "lab_bioq_i"] },
+  { id: "fisiologia", nombre: "Fisiología", creditos: 10, requisitos: ["bioquimica", "bases_fisicas"] },
+  { id: "lab_fisiologia", nombre: "Laboratorio de Fisiología", creditos: 5, requisitos: ["fisiologia"] },
+  { id: "quimica_fisica_i", nombre: "Química Física I", creditos: 10, requisitos: [] },
+
+  // CUARTO AÑO
+  { id: "quimica_fisica_ii", nombre: "Química Física II", creditos: 10, requisitos: ["quimica_fisica_i"] },
+  { id: "optativos_lic", nombre: "Optativos Profundización Licenciatura", creditos: 35, requisitos: [] },
+  { id: "opr_experiencia", nombre: "OPR Experiencia de Investigación", creditos: 15, requisitos: ["lab_bioq_i"] },
+  { id: "seminario_practica", nombre: "Seminario de Investigación o Práctica Extramural", creditos: 30, requisitos: ["lab_bioq_ii", "fisiologia", "lab_fisiologia", "opr_experiencia"] },
+  { id: "lab_clinico", nombre: "Laboratorio Clínico", creditos: 10, requisitos: ["lab_bioq_ii"] },
+
+  // QUINTO AÑO
+  { id: "tecnicas_avanzadas", nombre: "Técnicas Avanzadas en Bioquímica", creditos: 50, requisitos: [] },
+  { id: "optativos_titulo", nombre: "Optativos de Profundización Título", creditos: 50, requisitos: [] },
+  { id: "memoria_investigacion", nombre: "Memoria de Investigación", creditos: 50, requisitos: [] },
+  { id: "memoria_profesional", nombre: "Memoria Profesional", creditos: 50, requisitos: [] },
 ];
 
 let creditosTotales = 0;
@@ -93,10 +99,7 @@ function desbloquearRamosDependientes(idAprobado) {
   });
 }
 
-// Inicializa la malla
 ramos.forEach((r) => crearRamo(r));
-
-// Desbloquea los que no tienen requisitos
 ramos.forEach((r) => {
   if (r.requisitos.length === 0) {
     document.getElementById(r.id).classList.remove("bloqueado");
